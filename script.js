65635b01cea457adf907f4ce69f2c5ae3abf9838
Mukul Singh <135323609+Mukul1598@users.noreply.github.com>
2024-05-30 21:20:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function stringChop(str, size) {
 	if(!str) return [];
 
         const chunks = [];
-        for(let i = = 0; i<str.length; i = i + size){
+        for(let i = 0; i<str.length; i = i + size){
             chunks.push(str.slice(i , i + size))
         }
         return chunks;
